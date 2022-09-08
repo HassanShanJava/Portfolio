1bcde6e6bd10d08659ba02cc16ca0d7789048386
HassanShanJava <hassanshan675@gmail.com>
2022-09-08 18:34:53 +0500

    updated contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
+    <div name='contact' className='w-full h-full bg-[#0a192f] flex justify-center items-center p-4'>
         {/* for getpost.io */}
         <form method="POST" action='https://getform.io/f/5e6ec90b-c8bc-4161-8693-62f8f064a4f6' className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8 '>
@@ -15,7 +15,7 @@ const Contact = () => {
                    className='my-4 p-2 bg-[#ccd6f6]' />
             <input type='text' placeholder='Subject' name='Subject' 
                    className='my-4 p-2 bg-[#ccd6f6]' />
-            <textarea rows='10' placeholder='Message' name='message'
+            <textarea rows='7' placeholder='Message' name='message'
                       className='my-4 p-2  bg-[#ccd6f6]' ></textarea>
             <button className='text-white border-2 px-4 py-3 my-8 mx-auto flex items-center
                          hover:bg-pink-600 hover:border-pink-600'>Let's Collaborate</button>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -15,7 +15,7 @@ import Git from "../assets/git.png"
 
 const Skills = () => {
   return (
-    <div name='skills' className='w-full h-[150vh] bg-[#0a192f] text-gray-300'>
+    <div name='skills' className='w-full h-full  bg-[#0a192f] text-gray-300'>
       {/* coonatiner */}
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div>
