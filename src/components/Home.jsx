fc34a66d51c117b0b17aa9449899bd229e63de66
HassanShanJava <hassanshan675@gmail.com>
2022-09-11 12:52:58 +0500

    Fixed typewriter spacing

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
     // name property important for react-smooth scroll
     <div name="home" className="w-full h-screen bg-[#0a192f]">
       {/* Container */}
-      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full ">
+      <div className="max-w-[1100px] mx-auto px-8 flex flex-col justify-center h-full ">
         <p className="text-pink-600">Hi, my name is</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
           Hassan Shan
@@ -20,7 +20,7 @@ const Home = () => {
           I'm a 
           </h2>
           
-          <div className="ml-6 text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          <div className="ml-4 text-4xl sm:text-7xl font-bold text-[#8892b0]">
               <Typewriter
                 options={
                   {
