76c85f747c11aa5a04ad51bf258e6599ea6928ba
HassanShanJava <hassanshan675@gmail.com>
2022-09-11 23:14:03 +0500

    Added first project

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,7 +7,7 @@ import AptiveData from "../assets/snipcode/aptive-data.png";
 import Drawing from "../assets/snipcode/drawing-app.png";
 import Webull from "../assets/snipcode/react-styled-webull.png";
 import Datasite from "../assets/snipcode/datasite.png";
-
+import FirstResponsive from "../assets/snipcode/responsive-portfolio.png";
 
 const Portfolio = () => {
   const string2="// Check out some of my recent work.";
@@ -216,6 +216,38 @@ const Portfolio = () => {
               </div>
             </div>
           </div>
+
+          <div
+            style={{ backgroundImage: `url(${FirstResponsive})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md 
+                          flex justify-center text-center items-center mx-auto content-div "
+          >
+            {/* Hover effect for images */}
+            <div className="opacity-0 group-hover:opacity-100 ">
+              <span className="text-2xl font bold text-white tracking-wider ">
+                First Responsive Website
+              </span>
+              <div className="pt-8 text-center ">
+                <a href="https://github.com/HassanShanJava/my-project-portfolio" target="__blank">
+                  <button
+                    className="text-center rounded-lg px-4 py-3 m-2
+                                   bg-white text-gray-700 font-bold text-lg"
+                  >
+                    Code
+                  </button>
+                </a>
+                <a href="https://hassan-shan-portfolio.netlify.app/" target="__blank">
+                  <button
+                    className="text-center rounded-lg px-4 py-3 m-2
+                                   bg-white text-gray-700 font-bold text-lg"
+                  >
+                    Live
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
 
           <div
             style={{ backgroundImage: `url(${Webull})` }}
