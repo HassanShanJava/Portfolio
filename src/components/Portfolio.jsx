b6bc29d8dbc6af39b84c1ba417d378ad33f6ed9d
HassanShanJava <hassanshan675@gmail.com>
2022-09-24 13:43:59 +0500

    optimized portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,18 +1,14 @@
 import React from "react";
+import { useState } from "react";
 
-import AirClone from "../assets/snipcode/airbnb-clone.png";
-import ModBank from "../assets/snipcode/modern-bank.png";
-import GalaxyTravels from "../assets/snipcode/galaxy-travels.png";
-import AptiveData from "../assets/snipcode/aptive-data.png";
-import Drawing from "../assets/snipcode/drawing-app.png";
-import Webull from "../assets/snipcode/react-styled-webull.png";
-import Datasite from "../assets/snipcode/datasite.png";
-import FirstResponsive from "../assets/snipcode/responsive-portfolio.png";
-import FoodPanda from "../assets/snipcode/food-panda.png";
-import Drive from "../assets/snipcode/drive-rental.png";
+import { data } from "../data/data.js";
 
 const Portfolio = () => {
   const string2="// Check out some of my recent work.";
+
+  // projects file
+  const [project, setProject] = useState(data);
+  
   return (
     <div
       name="portfolio"
@@ -30,322 +26,41 @@ const Portfolio = () => {
         {/* container for projects */}
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           
-          {/* Gird Item */}
-          <div
-            style={{ backgroundImage: `url(${AirClone})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md 
-                          flex justify-center text-center items-center mx-auto content-div "
-          >
-            {/* Hover effect for images */}
-            <div className="opacity-0 group-hover:opacity-100 ">
-              <span className="text-2xl font bold text-white tracking-wider ">
-                Airbnb Landing Clone
-              </span>
-              <div className="pt-8 text-center ">
-                <a href="https://github.com/HassanShanJava/airbnb-landing-clone" target="__blank">
-                  <button
-                    className="text-center rounded-lg px-4 py-3 m-2
-                                   bg-white text-gray-700 font-bold text-lg"
-                  >
-                    Code
-                  </button>
-                </a>
-                <a href="https://airbnb-landing-clone-page.netlify.app/" target="_-blank">
-                  <button
-                    className="text-center rounded-lg px-4 py-3 m-2
-                                   bg-white text-gray-700 font-bold text-lg"
-                  >
-                    Live
-                  </button>
-                </a>
+                    {/* Gird Item */}
+                    {project.map((item, index) => (
+            <div
+              key={index}
+              style={{ backgroundImage: `url(${item.image})` }}
+              className="shadow-lg shadow-[#040c16] group container rounded-md 
+                        flex justify-center text-center items-center mx-auto content-div "
+            >
+              {/* Hover effect for images */}
+              <div className="opacity-0 group-hover:opacity-100 ">
+                <span className="text-2xl font bold text-white tracking-wider ">
+                  {item.name}
+                </span>
+                <div className="pt-8 text-center ">
+                  <a href={item.github} target="__blank">
+                    <button
+                      className="text-center rounded-lg px-4 py-3 m-2
+                                 bg-white text-gray-700 font-bold text-lg"
+                    >
+                      Code
+                    </button>
+                  </a>
+                  <a href={item.live} target="_-blank">
+                    <button
+                      className="text-center rounded-lg px-4 py-3 m-2
+                                 bg-white text-gray-700 font-bold text-lg"
+                    >
+                      Live
+                    </button>
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
+          ))}
 
-          <div
-            style={{ backgroundImage: `url(${GalaxyTravels})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md 
-                          flex justify-center text-center items-center mx-auto content-div "
-          >
-            {/* Hover effect for images */}
-            <div className="opacity-0 group-hover:opacity-100 ">
-              <span className="text-2xl font bold text-white  tracking-wider ">
-                Galaxy Travels Application
-              </span>
-              <div className="pt-8 text-center ">
-                <a href="https://github.com/HassanShanJava/galaxy-travels-router" target="__blank">
-                  <button
-                    className="text-center rounded-lg px-4 py-3 m-2
-                                   bg-white text-gray-700 font-bold text-lg"
-                  >
-                    Code
-                  </button>
-                </a>
-                <a href="https://galaxy-travel-page.netlify.app/" target="__blank">
-                  <button
-                    className="text-center rounded-lg px-4 py-3 m-2
-                                   bg-white text-gray-700 font-bold text-lg"
-                  >
-                    Live
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div
-            style={{ backgroundImage: `url(${ModBank})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md 
-                          flex justify-center text-center items-center mx-auto content-div "
-          >
-            {/* Hover effect for images */}
-            <div className="opacity-0 group-hover:opacity-100 ">
-              <span className="text-2xl font bold text-white tracking-wider ">
-                Modern Banking Appilcation
-              </span>
-              <div className="pt-8 text-center ">
-                <a href="https://github.com/HassanShanJava/modern-banking-app" target="__blank">
-                  <button
-                    className="text-center rounded-lg px-4 py-3 m-2
-                                   bg-white text-gray-700 font-bold text-lg"
-                  >
-                    Code
-                  </button>
-                </a>
-                <a href="https://hs-modern-bank-app.netlify.app/" target="__blank">
-                  <button
-                    className="text-center rounded-lg px-4 py-3 m-2
-                                   bg-white text-gray-700 font-bold text-lg"
-                  >
-                    Live
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div
-            style={{ backgroundImage: `url(${AptiveData})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md 
-                          flex justify-center text-center items-center mx-auto content-div "
-          >
-            {/* Hover effect for images */}
-            <div className="opacity-0 group-hover:opacity-100 ">
-              <span className="text-2xl font bold text-white tracking-wider ">
-                Aptive Data Application
-              </span>
-              <div className="pt-8 text-center ">
-                <a href="https://github.com/HassanShanJava/Aptive-Router" target="__blank">
-                  <button
-                    className="text-center rounded-lg px-4 py-3 m-2
-                                   bg-white text-gray-700 font-bold text-lg"
-                  >
-                    Code
-                  </button>
-                </a>
-                <a href="https://aptive-data-commerce.netlify.app/" target="__blank">
-                  <button
-                    className="text-center rounded-lg px-4 py-3 m-2
-                                   bg-white text-gray-700 font-bold text-lg"
-                  >
-                    Live
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-
-          {/* Add more projects next */}
-          <div
-            style={{ backgroundImage: `url(${Datasite})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md 
-                          flex justify-center text-center items-center mx-auto content-div "
-          >
-            {/* Hover effect for images */}
-            <div className="opacity-0 group-hover:opacity-100 ">
-              <span className="text-2xl font bold text-white tracking-wider ">
-                Data Site Application
-              </span>
-              <div className="pt-8 text-center ">
-                <a href="https://github.com/HassanShanJava/DataSite-Tail" target="__blank">
-                  <button
-                    className="text-center rounded-lg px-4 py-3 m-2
-                                   bg-white text-gray-700 font-bold text-lg"
-                  >
-                    Code
-                  </button>
-                </a>
-                <a href="https://datasite-rt.netlify.app/" target="__blank">
-                  <button
-                    className="text-center rounded-lg px-4 py-3 m-2
-                                   bg-white text-gray-700 font-bold text-lg"
-                  >
-                    Live
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-
-
-          <div
-            style={{ backgroundImage: `url(${Drawing})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md 
-                          flex justify-center text-center items-center mx-auto content-div "
-          >
-            {/* Hover effect for images */}
-            <div className="opacity-0 group-hover:opacity-100 ">
-              <span className="text-2xl font bold text-white tracking-wider ">
-                Drawing Application
-              </span>
-              <div className="pt-8 text-center ">
-                <a href="https://github.com/HassanShanJava/drawing-app" target="__blank">
-                  <button
-                    className="text-center rounded-lg px-4 py-3 m-2
-                                   bg-white text-gray-700 font-bold text-lg"
-                  >
-                    Code
-                  </button>
-                </a>
-                <a href="https://drawing-app-2.netlify.app/" target="__blank">
-                  <button
-                    className="text-center rounded-lg px-4 py-3 m-2
-                                   bg-white text-gray-700 font-bold text-lg"
-                  >
-                    Live
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div
-            style={{ backgroundImage: `url(${FirstResponsive})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md 
-                          flex justify-center text-center items-center mx-auto content-div "
-          >
-            {/* Hover effect for images */}
-            <div className="opacity-0 group-hover:opacity-100 ">
-              <span className="text-2xl font bold text-white tracking-wider ">
-                First Responsive Website
-              </span>
-              <div className="pt-8 text-center ">
-                <a href="https://github.com/HassanShanJava/my-project-portfolio" target="__blank">
-                  <button
-                    className="text-center rounded-lg px-4 py-3 m-2
-                                   bg-white text-gray-700 font-bold text-lg"
-                  >
-                    Code
-                  </button>
-                </a>
-                <a href="https://hassan-shan-portfolio.netlify.app/" target="__blank">
-                  <button
-                    className="text-center rounded-lg px-4 py-3 m-2
-                                   bg-white text-gray-700 font-bold text-lg"
-                  >
-                    Live
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-
-
-          <div
-            style={{ backgroundImage: `url(${Webull})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md 
-                          flex justify-center text-center items-center mx-auto content-div "
-          >
-            {/* Hover effect for images */}
-            <div className="opacity-0 group-hover:opacity-100 ">
-              <span className="text-2xl font bold text-white tracking-wider ">
-                Webull Stocks Application
-              </span>
-              <div className="pt-8 text-center ">
-                <a href="https://github.com/HassanShanJava/react_styled_components_gh" target="__blank">
-                  <button
-                    className="text-center rounded-lg px-4 py-3 m-2
-                                   bg-white text-gray-700 font-bold text-lg"
-                  >
-                    Code
-                  </button>
-                </a>
-                <a href="https://github.com/HassanShanJava/react_styled_components_gh" target="__blank">
-                  <button
-                    className="text-center rounded-lg px-4 py-3 m-2
-                                   bg-white text-gray-700 font-bold text-lg"
-                  >
-                    Live
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-
-
-          {/* Next Project */}
-          <div
-            style={{ backgroundImage: `url(${FoodPanda})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md 
-                          flex justify-center text-center items-center mx-auto content-div "
-          >
-            {/* Hover effect for images */}
-            <div className="opacity-0 group-hover:opacity-100 ">
-              <span className="text-2xl font bold text-white tracking-wider ">
-                FoodPanda Landing Page Clone
-              </span>
-              <div className="pt-8 text-center ">
-                <a href="https://github.com/HassanShanJava/FoodPanda-Clone-Landing-Page" target="__blank">
-                  <button
-                    className="text-center rounded-lg px-4 py-3 m-2
-                                   bg-white text-gray-700 font-bold text-lg"
-                  >
-                    Code
-                  </button>
-                </a>
-                <a href="https://foodpanda-clone.netlify.app/" target="__blank">
-                  <button
-                    className="text-center rounded-lg px-4 py-3 m-2
-                                   bg-white text-gray-700 font-bold text-lg"
-                  >
-                    Live
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div
-            style={{ backgroundImage: `url(${Drive})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md 
-                          flex justify-center text-center items-center mx-auto content-div "
-          >
-            {/* Hover effect for images */}
-            <div className="opacity-0 group-hover:opacity-100 ">
-              <span className="text-2xl font bold text-white tracking-wider ">
-                Drive Cars Rental Application 
-              </span>
-              <div className="pt-8 text-center ">
-                <a href="https://github.com/HassanShanJava/drive-cars-landing-page" target="__blank">
-                  <button
-                    className="text-center rounded-lg px-4 py-3 m-2
-                                   bg-white text-gray-700 font-bold text-lg"
-                  >
-                    Code
-                  </button>
-                </a>
-                <a href="https://drive-rent-a-car.netlify.app/" target="__blank">
-                  <button
-                    className="text-center rounded-lg px-4 py-3 m-2
-                                   bg-white text-gray-700 font-bold text-lg"
-                  >
-                    Live
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          
 
         </div>
       </div>
---
+++ src/data/data.js
@@ -0,0 +1,91 @@
+import AirClone from "../assets/snipcode/airbnb-clone.png";
+import ModBank from "../assets/snipcode/modern-bank.png";
+import GalaxyTravels from "../assets/snipcode/galaxy-travels.png";
+import AptiveData from "../assets/snipcode/aptive-data.png";
+import Drawing from "../assets/snipcode/drawing-app.png";
+import Webull from "../assets/snipcode/react-styled-webull.png";
+import Datasite from "../assets/snipcode/datasite.png";
+import FirstResponsive from "../assets/snipcode/responsive-portfolio.png";
+import FoodPanda from "../assets/snipcode/food-panda.png";
+import Drive from "../assets/snipcode/drive-rental.png";
+import FoodStore from "../assets/snipcode/best-eats.png"
+
+export const data=[
+    {
+        id:1,
+        name:"Airbnb Landing Clone",
+        image:AirClone,
+        github:"https://github.com/HassanShanJava/airbnb-landing-clone",
+        live:"https://airbnb-landing-clone-page.netlify.app/",
+    },
+    {
+        id:2,
+        name:"Galaxy Travels Application",
+        image:GalaxyTravels,
+        github:"https://github.com/HassanShanJava/galaxy-travels-router",
+        live:"https://galaxy-travel-page.netlify.app/",
+    },
+    {
+        id:3,
+        name:"Modern Banking Appilcation",
+        image:ModBank,
+        github:"https://github.com/HassanShanJava/modern-banking-app",
+        live:"https://hs-modern-bank-app.netlify.app/",
+    },
+    {
+        id:4,
+        name:"Aptive Data Application",
+        image:AptiveData,
+        github:"https://github.com/HassanShanJava/Aptive-Router",
+        live:"https://aptive-data-commerce.netlify.app/",
+    },
+    {
+        id:5,
+        name:"Airbnb Landing Clone",
+        image:Datasite,
+        github:"https://github.com/HassanShanJava/airbnb-landing-clone",
+        live:"https://airbnb-landing-clone-page.netlify.app/",
+    },
+    {
+        id:6,
+        name:"Data Site Application",
+        image:Drawing,
+        github:"https://github.com/HassanShanJava/drawing-app",
+        live:"https://drawing-app-2.netlify.app/",
+    },
+    {
+        id:7,
+        name:"First Responsive Website",
+        image:FirstResponsive,
+        github:"https://github.com/HassanShanJava/my-project-portfolio",
+        live:"https://hassan-shan-portfolio.netlify.app/",
+    },
+    {
+        id:8,
+        name:"Webull Stocks Application",
+        image:Webull,
+        github:"https://github.com/HassanShanJava/react_styled_components_gh",
+        live:"https://hassanshanjava.github.io/react_styled_components_gh/",
+    },
+    {
+        id:9,
+        name:"FoodPanda Landing Page Clone",
+        image:FoodPanda,
+        github:"https://github.com/HassanShanJava/FoodPanda-Clone-Landing-Page",
+        live:"https://foodpanda-clone.netlify.app/",
+    },
+    {
+        id:10,
+        name:"Drive Cars Rental Application",
+        image:Drive,
+        github:"https://github.com/HassanShanJava/drive-cars-landing-page",
+        live:"https://drive-rent-a-car.netlify.app/",
+    },
+    {
+        id:11,
+        name:"Best Eats Application",
+        image:FoodStore,
+        github:"https://github.com/HassanShanJava/food-store",
+        live:"https://best-eats-foodstore.netlify.app/",
+    },
+]
