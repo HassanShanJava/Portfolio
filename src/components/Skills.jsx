a4c59a64c83c183990cb2370c69eefe5d1ded03f
HassanShanJava <hassanshan675@gmail.com>
2022-11-26 08:50:52 +0500

    added skill typescript

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,7 +8,7 @@ const Skills = () => {
   const skills = dataIcons;
 
   return (
-    <div name="skills" className="w-full h-screen   bg-[#0a192f] text-gray-300">
+    <div name="skills" className="w-full h-full   bg-[#0a192f] text-gray-300">
       {/* coonatiner */}
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
