89a063c9b14ba5b6dbfc11cbe18fa11a39b76369
HassanShanJava <hassanshan675@gmail.com>
2023-05-04 04:33:12 +0500

    added banner message

---
+++ src/components/Banner.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <>
+      <div className="sticky top-0 bg-purple-800 p-2 text-slate-200 w-full ">
+        <p className="text-center animate-pulse">
+          Exciting changes are coming to our website! Please pardon the
+          appearance while I work behind the scenes.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Banner;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,65 +15,69 @@ import {
 
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
+import Banner from "./Banner";
 
 const NavBar = () => {
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
 
   return (
-    <div name="/"
-      className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300"
-    >
-      <div className="logo">
-        <Link to="/" smooth={true} offset={50} duration={500}>
-          <img
-            className="cursor-pointer"
-            src={Logo}
-            alt="/"
-            style={{ width: "120px", marginTop: "20px" }}
-          />
-        </Link>
-      </div>
+    <>
+      <Banner />
+      <div
+        name="/"
+        className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300"
+      >
+        <div className="logo">
+          <Link to="/" smooth={true} offset={50} duration={500}>
+            <img
+              className="cursor-pointer"
+              src={Logo}
+              alt="/"
+              style={{ width: "120px", marginTop: "20px" }}
+            />
+          </Link>
+        </div>
 
-      {/* Menu 
+        {/* Menu 
       
          md->anything above medium      
       
       */}
-      <ul className="hidden md:flex">
-        <li>
-          <Link to='' smooth={true} offset={50} duration={500}>
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="about" smooth={true} offset={50} duration={500}>
-            About
-          </Link>
-        </li>
-        <li>
-          <Link to="skills" smooth={true} offset={50} duration={500}>
-            Skills
-          </Link>
-        </li>
-        <li>
-          <Link to="portfolio" smooth={true} offset={50} duration={500}>
-            Portfolio
-          </Link>
-        </li>
-        <li>
-          <Link to="contact" smooth={true} offset={50} duration={500}>
-            Contact
-          </Link>
-        </li>
-      </ul>
+        <ul className="hidden md:flex">
+          <li>
+            <Link to="" smooth={true} offset={50} duration={500}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="about" smooth={true} offset={50} duration={500}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="skills" smooth={true} offset={50} duration={500}>
+              Skills
+            </Link>
+          </li>
+          <li>
+            <Link to="portfolio" smooth={true} offset={50} duration={500}>
+              Portfolio
+            </Link>
+          </li>
+          <li>
+            <Link to="contact" smooth={true} offset={50} duration={500}>
+              Contact
+            </Link>
+          </li>
+        </ul>
 
-      {/* Hamburger */}
-      <div onClick={handleClick} className="md:hidden z-10">
-        {nav ? <FaTimes /> : <FaBars />}
-      </div>
+        {/* Hamburger */}
+        <div onClick={handleClick} className="md:hidden z-10">
+          {nav ? <FaTimes /> : <FaBars />}
+        </div>
 
-      {/* menu 
+        {/* menu 
 
         h-> we want height:100vh ->> h-screen
         display:flex;
@@ -81,116 +85,148 @@ const NavBar = () => {
       
       */}
 
-      {/* Need to toggle the hamburger to move menu, -> useState */}
-      <ul
-        className={
-          nav
-            ? "absolute top-0 left-0 duration-200 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center "
-            : "absolute top-0 left-[-100%] duration-200 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center "
-        }
-      >
-        <li className="py-6 text-4xl">
-          <Link onClick={handleClick} to="" smooth={true} offset={50} duration={500}>
-            Home
-          </Link>
-        </li>
-        <li className="py-6 text-4xl">
-          <Link onClick={handleClick} to="about" smooth={true} offset={50} duration={500}>
-            About
-          </Link>
-        </li>
-        <li className="py-6 text-4xl">
-          <Link onClick={handleClick} to="skills" smooth={true} offset={50} duration={500}>
-            Skills
-          </Link>
-        </li>
-        <li className="py-6 text-4xl">
-          <Link onClick={handleClick} to="portfolio" smooth={true} offset={50} duration={500}>
-            Portfolio
-          </Link>
-        </li>
-        <li className="py-6 text-4xl">
-          <Link onClick={handleClick} to="contact" smooth={true} offset={50} duration={500}>
-            Contact
-          </Link>
-        </li>
-      </ul>
-
-      {/* social icons */}
-      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
-        <ul>
-          <li
-            className="w-[160px] h-[60px] flex justify-between
-                         items-center ml-[-100px] hover:ml-[-10px] duration-300
-                         bg-blue-600"
-          >
-            {/* eslint-disable-next-line */}
-            <a
-              className="flex justify-between items-center w-full text-gray-300"
-              href="https://www.linkedin.com/in/hassan-shan/"
-              target="_blank"
+        {/* Need to toggle the hamburger to move menu, -> useState */}
+        <ul
+          className={
+            nav
+              ? "absolute top-0 left-0 duration-200 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center "
+              : "absolute top-0 left-[-100%] duration-200 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center "
+          }
+        >
+          <li className="py-6 text-4xl">
+            <Link
+              onClick={handleClick}
+              to=""
+              smooth={true}
+              offset={50}
+              duration={500}
             >
-              LinkedIn <FaLinkedin size={30} />
-            </a>
+              Home
+            </Link>
           </li>
-          <li
-            className="w-[160px] h-[60px] flex justify-between
-                         items-center ml-[-100px] hover:ml-[-10px] duration-300
-                         bg-[#333]"
-          >
-            {/* eslint-disable-next-line */}
-            <a
-              className="flex justify-between items-center w-full text-gray-300"
-              href="https://github.com/HassanShanJava"
-              target="_blank"
+          <li className="py-6 text-4xl">
+            <Link
+              onClick={handleClick}
+              to="about"
+              smooth={true}
+              offset={50}
+              duration={500}
             >
-              Github <FaGithub size={30} />
-            </a>
+              About
+            </Link>
           </li>
-          <li
-            className="w-[160px] h-[60px] flex justify-between
-                         items-center ml-[-100px] hover:ml-[-10px] duration-300
-                         bg-[#6fc2b0]"
-          >
-            {/* eslint-disable-next-line */}
-            <a
-              className="flex justify-between items-center w-full text-gray-300"
-              href="mailto: hassanshan675@gmail.com" target='_blank'
-             >
-              Email <HiOutlineMail size={30} />
-            </a>
-          </li>
-          <li
-            className="w-[160px] h-[60px] flex justify-between
-                         items-center ml-[-100px] hover:ml-[-10px] duration-300
-                         bg-[#565f69]"
-          >
-            {/* eslint-disable-next-line */}
-            <a
-              className="flex justify-between items-center w-full text-gray-300"
-              href="https://acrobat.adobe.com/link/review?uri=urn:aaid:scds:US:228933e2-44a5-4d53-9e2e-081b19114d94"
-              target="_blank"
+          <li className="py-6 text-4xl">
+            <Link
+              onClick={handleClick}
+              to="skills"
+              smooth={true}
+              offset={50}
+              duration={500}
             >
-              Resume <BsFillPersonLinesFill size={30} />
-            </a>
+              Skills
+            </Link>
           </li>
-          <li
-            className="w-[160px] h-[60px] flex justify-between
-                         items-center ml-[-100px] hover:ml-[-10px] duration-300
-                         bg-[#c91f33]"
-          >
-            {/* eslint-disable-next-line */}
-            <a
-              className="flex justify-between items-center w-full text-gray-300"
-              href="https://www.youtube.com/@hassanshan1270"
-              target="_blank"
+          <li className="py-6 text-4xl">
+            <Link
+              onClick={handleClick}
+              to="portfolio"
+              smooth={true}
+              offset={50}
+              duration={500}
             >
-              YouTube <FaYoutube size={30} />
-            </a>
+              Portfolio
+            </Link>
+          </li>
+          <li className="py-6 text-4xl">
+            <Link
+              onClick={handleClick}
+              to="contact"
+              smooth={true}
+              offset={50}
+              duration={500}
+            >
+              Contact
+            </Link>
           </li>
         </ul>
+
+        {/* social icons */}
+        <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+          <ul>
+            <li
+              className="w-[160px] h-[60px] flex justify-between
+                         items-center ml-[-100px] hover:ml-[-10px] duration-300
+                         bg-blue-600"
+            >
+              {/* eslint-disable-next-line */}
+              <a
+                className="flex justify-between items-center w-full text-gray-300"
+                href="https://www.linkedin.com/in/hassan-shan/"
+                target="_blank"
+              >
+                LinkedIn <FaLinkedin size={30} />
+              </a>
+            </li>
+            <li
+              className="w-[160px] h-[60px] flex justify-between
+                         items-center ml-[-100px] hover:ml-[-10px] duration-300
+                         bg-[#333]"
+            >
+              {/* eslint-disable-next-line */}
+              <a
+                className="flex justify-between items-center w-full text-gray-300"
+                href="https://github.com/HassanShanJava"
+                target="_blank"
+              >
+                Github <FaGithub size={30} />
+              </a>
+            </li>
+            <li
+              className="w-[160px] h-[60px] flex justify-between
+                         items-center ml-[-100px] hover:ml-[-10px] duration-300
+                         bg-[#6fc2b0]"
+            >
+              {/* eslint-disable-next-line */}
+              <a
+                className="flex justify-between items-center w-full text-gray-300"
+                href="mailto: hassanshan675@gmail.com"
+                target="_blank"
+              >
+                Email <HiOutlineMail size={30} />
+              </a>
+            </li>
+            <li
+              className="w-[160px] h-[60px] flex justify-between
+                         items-center ml-[-100px] hover:ml-[-10px] duration-300
+                         bg-[#565f69]"
+            >
+              {/* eslint-disable-next-line */}
+              <a
+                className="flex justify-between items-center w-full text-gray-300"
+                href="https://acrobat.adobe.com/link/review?uri=urn:aaid:scds:US:228933e2-44a5-4d53-9e2e-081b19114d94"
+                target="_blank"
+              >
+                Resume <BsFillPersonLinesFill size={30} />
+              </a>
+            </li>
+            <li
+              className="w-[160px] h-[60px] flex justify-between
+                         items-center ml-[-100px] hover:ml-[-10px] duration-300
+                         bg-[#c91f33]"
+            >
+              {/* eslint-disable-next-line */}
+              <a
+                className="flex justify-between items-center w-full text-gray-300"
+                href="https://www.youtube.com/@hassanshan1270"
+                target="_blank"
+              >
+                YouTube <FaYoutube size={30} />
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
