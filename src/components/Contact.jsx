e70ab1317822c8fd851088dcf2cf86007b98f17c
Hassan Shan <hassanshan675@gmail.com>
2024-01-18 11:38:43 +0500

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
   return (
     <div
       name="contact"
-      className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4"
+      className="w-full h-fit bg-[#0a192f] flex justify-center items-center p-4"
     >
       {/* for getpost.io */}
       <form
